417d430228e1efe2d3c58500a26c7e4fe45c728c
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-11-03 12:27:02 +0100

    Get Offer table

--- src/Controller/CreateOfferController.php
+++ src/Controller/CreateOfferController.php
@@ -8,6 +8,46 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CreateOfferController extends AbstractController
 {
+    protected $title;
+    protected $enterprise;
+    protected $description;
+    protected $html = "HTML/CSS";
+    protected $js = "JS";
+    protected $java = "Java";
+    protected $php = "PHP";
+    protected $laravel = "Laravel";
+    protected $symfony = "Symfony";
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getEnterprise(): string
+    {
+        return $this->Enterprise;
+    }
+
+    public function setEnterprise(string $enterprise): void
+    {
+        $this->enterprise = $enterprise;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
     #[Route('/create/offer', name: 'app_create_offer')]
     public function index(): Response
     {
@@ -17,9 +57,10 @@ class CreateOfferController extends AbstractController
     }
 
     public function createJob(Response $response){
-        $job = new Job();
-        $job = setTitle($response.title);
-        $job = setDescription($response.description);
+        // $job = new Job();
+        // $job = setTitle($response.title);
+        // $job = setEnterprise($response.value["enterprise"])
+        // $job = setDescription($response.description);
     }
 
     public function createSkills(Response $response){
