045b744f3d6c29a820f9a63e60be944d06b35321
Zook-coder <Zooklenon11@gmail.com>
2022-11-03 21:40:05 +0100

    add fixtures

--- migrations/Version20221103191709.php
+++ migrations/Version20221103191709.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221103143958 extends AbstractMigration
+final class Version20221103191709 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20221103143958 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE skills DROP FOREIGN KEY FK_D531167048704627');
-        $this->addSql('DROP INDEX IDX_D531167048704627 ON skills');
-        $this->addSql('ALTER TABLE skills DROP jobs_id');
+        $this->addSql('ALTER TABLE business CHANGE phone phone VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE skills ADD jobs_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE skills ADD CONSTRAINT FK_D531167048704627 FOREIGN KEY (jobs_id) REFERENCES jobs (id)');
-        $this->addSql('CREATE INDEX IDX_D531167048704627 ON skills (jobs_id)');
+        $this->addSql('ALTER TABLE business CHANGE phone phone INT NOT NULL');
     }
 }
