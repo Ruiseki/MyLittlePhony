6c10a6c48ecbf6f19162e2269026922e00c48174
Zook-coder <Zooklenon11@gmail.com>
2022-11-04 10:54:51 +0100

    controller creation

---
+++ src/Controller/DisplayBusinessesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BusinessRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DisplayBusinessesController extends AbstractController
+{
+    #[Route('/display/businesses', name: 'app_display_businesses')]
+    public function index(BusinessRepository $businessRepository): Response
+    {
+        $result = $businessRepository->findAll();
+        return $this->render('display_businesses/index.html.twig', [
+            'controller_name' => 'DisplayBusinessesController',
+        ]);
+    }
+}
