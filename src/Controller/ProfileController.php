ee234412065b7e2d354aa2e165bc9238f541fd98
Ruiseki <jojobru60@gmail.com>
2022-11-04 10:29:46 +0100

    Deleting useless file + rename

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
+use App\Form\OfferFormType;
+use App\Repository\ProfilsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_userProfile')]
-    public function index(): Response
+    public function index(ProfilsRepository $profileRepository, Request $request, EntityManagerInterface $entityMgr): Response
     {
+        $profil = new Profile();
+        
+
         return $this->render('userProfile/index.html.twig', [
             'controller_name' => 'UserProfileController',
         ]);
