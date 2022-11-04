23828cfbb2389b7685246aded6c8b25b8eaa64a0
Zook-coder <Zooklenon11@gmail.com>
2022-11-04 10:03:45 +0100

    Add offers visibility

--- src/Controller/DisplayOffersController.php
+++ src/Controller/DisplayOffersController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\JobsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisplayOffersController extends AbstractController
-{   
+{
     #[Route('/offers', name: 'app_display_offers')]
-    public function index(): Response
+    public function index(JobsRepository $jobsRepository): Response
     {
-
+        $result = $jobsRepository->findAll();
         return $this->render('display_offers/index.html.twig', [
             'controller_name' => 'Offers',
-            'offers' => array('offers1', 'offers2')
+            'offers' => $result,
         ]);
     }
 }
