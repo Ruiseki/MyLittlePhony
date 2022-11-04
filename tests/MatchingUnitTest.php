ecebcc28fbdade145a0c1583c5066a7aa329a24c
N0ku <lucielecouturier2@gmail.com>
2022-11-04 10:12:13 +0100

    update test

--- tests/MatchingUnitTest.php
+++ tests/MatchingUnitTest.php
@@ -3,15 +3,34 @@
 namespace App\Tests;
 
 use App\Entity\Jobs;
+use App\Entity\Business;
 use PHPUnit\Framework\TestCase;
 
 class MatchingUnitTest extends TestCase
 {
-    public function testSomething(): void
+    public function testTrue(): void
     {
         $job = new Jobs();
+        $bus = new Business();
+        $bus->setName("SPACE-1222 Inc.");
+        $bus->setPhone("03 65 13 12 22");
 
-        $job->setName(('titre'));
-        $this->assertTrue(true);
+        $job->setName(('titre'))->setDescription("lorem description de l'offre")->setBusiness($bus);
+        $this->assertTrue($job->getBusiness() === $bus);
+        $this->assertTrue($job->getName() === 'titre');
+        $this->assertTrue($job->getDescription() === "lorem description de l'offre");
+    }
+    public function testFalse(): void
+    {
+        $job = new Jobs();
+        $bus = new Business();
+        $bus->setName("SPACE-1222 Inc.");
+        $bus->setPhone("03 65 13 12 22");
+
+        $job->setName(('titre'))->setDescription("lorem description de l'offre")->setBusiness($bus);
+        $this->assertFalse($job->getBusiness() === 'false');
+        $this->assertFalse($job->getBusiness() === 'false');
+        $this->assertFalse($job->getName() === 'false');
+        $this->assertFalse($job->getDescription() === 'false');
     }
 }
