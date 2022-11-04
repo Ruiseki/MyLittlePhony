84b2843fb5a1c5d3a3b5cbc02913fb47bf51dc6a
Ruiseki <jojobru60@gmail.com>
2022-11-04 15:43:28 +0100

    .
    Co-authored-by: Killian.V <KillianVendewinkele@users.noreply.github.com>
    Co-authored-by: Nakku <lucielecouturier2@gmail.com>

--- src/Form/ProfileFormType.php
+++ src/Form/ProfileFormType.php
@@ -3,8 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Profile;
-use App\Repository\SkillsRepository;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,6 +15,36 @@ class ProfileFormType extends AbstractType
     {
         $builder
             ->add('name')
+/*             ->add('skills', ChoiceType::class, [
+                'multiple' => true,
+                'expanded' => true,
+                'choices' => [
+                    'Management' => '1',
+                    'Communication' => '2',
+                    'Git' => '3',
+                    'Java' => '4',
+                    'JavaScript' => '5',
+                    'Python' => '6',
+                    'Android' => '7',
+                    'IOS' => '8',
+                    'Web' => '9',
+                    'App' => '10',
+                    'Mobile' => '11',
+                    'Windows' => '12',
+                    'Linux' => '13',
+                    'Unix' => '14',
+                    'Bash' => '15',
+                    'Crypto' => '16',
+                    'English' => '17',
+                    'Spanish' => '18',
+                    'German' => '19',
+                    'Italian' => '20',
+                    'Chinese' => '21',
+                    'Arabic' => '22',
+                    'Polish' => '23',
+                    'Oral expression' => '24',
+                ],
+            ]) */
             ->add('Create', SubmitType::class)
         ;
     }
